abf5f590369c83ac70ee9b167d54becccc4ebd9a
shubo7996 <whatsupmofo420@gmail.com>
2018-07-07 04:43:19 +0530

    account edit

--- reddit_bot.py
+++ reddit_bot.py
@@ -6,14 +6,14 @@ def bot_login():
             password = config.password,
             client_id = config.client_id,
             client_secret = config.client_secret,
-            user_agent = "sassysalmnder testing lsd picture v0.1" )
+            user_agent = "what_a_mofo's testing lsd picture v0.1" )
     return r
 
 def run_bot(r):
     for comment in r.subreddit('LSD').comments(limit = 50):
-        if 'lsd' in comment.body":
+        if "lsd" in comment.body:
             print ("String Found!")
 
 
 r = bot_login()
-run_bot()
+run_bot(r)
